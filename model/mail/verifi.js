ee968386b60038757ce196a31299d598df81242e
Hüseyin Yalçın <hsyndvlpr@gmail.com>
2024-05-28 20:14:37 +0300

    aa
    aaa

--- model/mail/verifi.js
+++ model/mail/verifi.js
@@ -1,6 +1,5 @@
 const nodemailer = require("nodemailer");
-let yusuf=22
-let hsyn = 15
+
 let verifiFunc = (code, mail) => {
   return new Promise((resolve, reject) => {
     let transporter = nodemailer.createTransport({
